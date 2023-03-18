76a9828ba3f63da8274a38bf50c9160fdb8d2b22
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-03-18 07:32:06 -0300

    Update __init__.py

--- caixa_mercadinho/__init__.py
+++ caixa_mercadinho/__init__.py
@@ -5,28 +5,31 @@ from tkinter import messagebox
 class Caixa_mercadinho:
     def __init__(self):
         self.janela_principal = Tk()
-        self.frame_1 = Frame(self.janela_principal, height=100, width=100)
+        self.janela_registradora = Tk()
+        self.frame_1 = Frame(self.janela_principal, height=250, width=600)
         self.frame_1.pack(side='left')
-        self.frame_2 = Frame(self.janela_principal, height=100, width=100)
-        self.frame_2.pack(side='left')
+        self.frame_2 = Frame(self.janela_principal, height=100, width=200)
+        self.frame_2.pack(side='right')
+        self.frame_1_REG = Frame(self.janela_registradora, height=100, width=200)
+        self.frame_1_REG.pack(side='left')
 
         self.radio_valor = IntVar()
         self.radio_valor.set(0)
 
-        self.label_1 = Label(self.frame_1, text='Selecione uma opção!')
-        self.label_1.pack()
+        self.label_1 = Label(self.frame_1, height=10, width=30, text='Selecione uma opção!')
+        self.label_1.pack(side='left')
 
-        self.botao_RD_entrar = Radiobutton(self.frame_1, text='Entrar', variable=self.radio_valor, value=1)
-        self.botao_RD_entrar.pack()
+        self.botao_RD_entrar = Radiobutton(self.frame_2, text='Entrar', variable=self.radio_valor, value=1)
+        self.botao_RD_entrar.pack(anchor='w')
 
-        self.botao_RD_sair = Radiobutton(self.frame_1, text='Sair', variable=self.radio_valor, value=2)
-        self.botao_RD_sair.pack()
+        self.botao_RD_sair = Radiobutton(self.frame_2, text='Sair', variable=self.radio_valor, value=2)
+        self.botao_RD_sair.pack(anchor='w')
 
-        self.botao_RD_enter = Button(self.frame_1, text='OK', command=self.radio_entrar)
-        self.botao_RD_enter.pack(side='left')
-        self.botao_DR_sair = Button(self.frame_1, text='Sair', command=self.janela_principal.quit)
+        self.botao_DR_sair = Button(self.frame_2, text='Sair', height=1, width=2, command=self.janela_principal.quit)
+        self.botao_DR_sair.pack(side='right')
 
-
+        self.botao_RD_enter = Button(self.frame_2, text='OK', height=2, width=4, command=self.radio_entrar)
+        self.botao_RD_enter.pack(side='right')
 
 
 
@@ -34,8 +37,7 @@ class Caixa_mercadinho:
         mainloop()
 
     def radio_entrar(self):
-        entrar_caixa = str(self.radio_valor.get())
-        messagebox.showinfo(f'Aviso!', f'Você escolheu a opção: "{entrar_caixa}"')
+        
 
 
 exec_caixa = Caixa_mercadinho()
